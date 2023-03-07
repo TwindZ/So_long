2abd1a87fc784e0be97a4534ff82c87f62705f90
TwindZ <emmanuel.lamothe@gmail.com>
2023-03-07 18:07:27 -0500

    yeah ready !

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:51:40 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/03/07 17:44:10 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/03/07 18:04:04 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,21 @@ void	ft_game(t_data *data)
 	ft_free_texture(data);
 }
 
-void	ft_map_info(t_data *data)
-{
-	ft_printf("Nombre de lignes = %d\n", data->line);
-	ft_printf("Nombre de collones = %d\n", data->col);
-	ft_printf("Nombre de players = %d\n", data->player);
-	ft_printf("Nombre de enemies = %d\n", data->enemycount);
-	ft_printf("Nombre de exit = %d\n", data->exit);
-	ft_printf("Nombre de collect = %d\n", data->collect);
-	ft_printf("Nombre de mauvais Char = %d\n", data->wrongchar);
-	ft_printf("Map rectangulaire = %d\n", data->rectangle);
-	ft_printf("Map valide = %d\n", data->valid);
-	ft_printf("Map walled = %d\n", data->walled);
-	ft_printf("Map flood exit = %d\n", data->flood.exit);
-	ft_printf("Map flood collect = %d\n", data->flood.collect);
-}
+// void	ft_map_info(t_data *data)
+// {
+// 	ft_printf("Nombre de lignes = %d\n", data->line);
+// 	ft_printf("Nombre de collones = %d\n", data->col);
+// 	ft_printf("Nombre de players = %d\n", data->player);
+// 	ft_printf("Nombre de enemies = %d\n", data->enemycount);
+// 	ft_printf("Nombre de exit = %d\n", data->exit);
+// 	ft_printf("Nombre de collect = %d\n", data->collect);
+// 	ft_printf("Nombre de mauvais Char = %d\n", data->wrongchar);
+// 	ft_printf("Map rectangulaire = %d\n", data->rectangle);
+// 	ft_printf("Map valide = %d\n", data->valid);
+// 	ft_printf("Map walled = %d\n", data->walled);
+// 	ft_printf("Map flood exit = %d\n", data->flood.exit);
+// 	ft_printf("Map flood collect = %d\n", data->flood.collect);
+// }
 
 int	main(int argc, char **argv)
 {
@@ -69,7 +69,6 @@ int	main(int argc, char **argv)
 	ft_map_data(data);
 	ft_validate(data);
 	ft_message(data);
-	ft_map_info(data);
 	if (data->valid == 0)
 	{
 		ft_game(data);
